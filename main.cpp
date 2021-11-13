36dfd699f998b48114223c43765fd9e513ae6c2a
and028 <91878390+and028@users.noreply.github.com>
2021-11-13 17:54:04 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
+    int n=0, i=0, numero1=0, numero2=1, successivo=0;
+    cin>>n;
+
+    for (i=1; i<=n; i++){
+
+        if (successivo<=n){
+
+            cout<<numero2<<endl;
+            successivo=numero1+numero2;
+            numero1=numero2;
+            numero2=successivo;
+        }
+
+    }
+
+
+
+        return 0;
 }
